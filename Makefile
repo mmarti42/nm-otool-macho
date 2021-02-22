4a3f4c05af200c62a3e876eadf099ce67e2ea1ab
Many Marti <mmarti@student.21-school.ru>
2021-02-22 17:55:50 +0300

    finish otool for 64 bit files (i'm sorry for that print, seriously)

--- Makefile
+++ Makefile
@@ -6,20 +6,20 @@ NM_DIR := nm
 
 OTOOL_DIR := otool
 
-all: $(NM) #$(OTOOL)
+all: $(NM) $(OTOOL)
 
 $(NM):
 	make -C $(NM_DIR)
 
-#$(OTOOL:):
-#	make -C $(OTOOL_DIR)
+$(OTOOL):
+	make -C $(OTOOL_DIR)
 
 clean:
 	make clean -C $(NM_DIR)
-	#make clean -C $(OTOOL_DIR)
+	make clean -C $(OTOOL_DIR)
 
 fclean:
 	make fclean -C $(NM_DIR)
-	#make fclean -C $(OTOOL_DIR)
+	make fclean -C $(OTOOL_DIR)
 
 re: fclean all
--- otool/otool.c
+++ otool/otool.c
@@ -30,12 +30,67 @@ int get_text64(t_text *buf, t_mach_header *mapped)
 	return -1;
 }
 
+int get_text32(t_text *buf, t_mach_header *mapped)
+{
+	struct segment_command *seg;
+	struct section *sect;
+	uint32_t i;
+
+	i = 0;
+	seg = (struct segment_command *)get_lc(LC_SEGMENT, mapped);
+	while (1)
+	{
+		if (!seg)
+			return -1;
+		if (!ft_strcmp(seg->segname, "__TEXT"))
+			break;
+		seg = (struct segment_command *)next_com((t_load_command *)seg);
+	}
+	sect = (struct section *)((char *)seg + sizeof(struct segment_command));
+	while (i++ < seg->nsects)
+	{
+		if (!ft_strcmp(sect->sectname, "__text"))
+		{
+			buf->text = (char *)mapped + sect->offset;
+			buf->size = sect->size;
+			return 0;
+		}
+		sect++;
+	}
+	return -1;
+}
+
 int	get_text(t_text *buf, t_mach_header *mapped)
 {
-//	if (g_file_type == archx64)
+	if (g_file_type == archx64)
 		return get_text64(buf, mapped);
-//	else
-//		return get_text32(buf, mapped);
+	else
+		return get_text32(buf, mapped);
+}
+
+void print_text(const unsigned char *text, size_t size)
+{
+	if (!size)
+		return;
+	while (size > 16)
+	{
+		ft_printf("%016llx ", text);
+		ft_printf("%02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x\n", text[0], text[1], text[3], text[4]\
+		 ,text[5], text[6], text[7], text[8], text[9], text[10], text[11]\
+		 ,text[12], text[13], text[14], text[15]);
+		 text += 16;
+		 size -= 16;
+	}
+	if (!size)
+		return;
+	ft_printf("%016llx ", text);
+	while (size)
+	{
+		ft_printf("%02x ", *text);
+		size--;
+		text++;
+	}
+	ft_putchar('\n');
 }
 
 void print_text_sect(char *fname)
@@ -57,7 +112,7 @@ void print_text_sect(char *fname)
 		g_file_type = mapped->magic;
 		if (get_text(&text, mapped) < 0)
 			ft_putstr_fd("Can't find text sect\n", STDERR_FILENO);
-		//print_text(&text);
+		print_text((const unsigned char *)text.text, text.size);
 	}
 	if (munmap(mapped_tmp, g_cfsize) < 0)
 		return (fatal_err(strerror(errno)));
